2d6cefdb37c3c97e384433ca78bff80c55942004
Daniel Waldmann <d.waldmann@it-dw.com>
2022-12-04 19:52:24 +0000

    „run.sh“ ändern

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 python manage.py migrate
 python manage.py loaddata seeds/station_seed.json
 python manage.py createcachetable
+python manage.py collectstatic
 gunicorn --bind 0.0.0.0:8000 main.wsgi
