dfe09886b6787fb968b28b57aac8341d821a6f01
Daniel Waldmann <25527351+dwaldmannDE@users.noreply.github.com>
2022-12-10 12:49:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ python manage.py migrate
 python manage.py createcachetable
 python manage.py loaddata seeds/station_seed.json
 python manage.py initadmin
-gunicorn --bind 0.0.0.0:8000 main.wsgi
+gunicorn --bind 0.0.0.0:8080 main.wsgi
